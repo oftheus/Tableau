1e1d4d071ad802cd2255adcbb8733ef942847fc4
Matheus <90159525+oftheus@users.noreply.github.com>
2024-06-06 15:58:52 -0300

    v 1.4
    v 1.4

--- main.hs
+++ main.hs
@@ -1,92 +1,31 @@
-import Data.Char (isAlpha)
+import Control.Applicative (liftA2)
+import Text.Parsec
+import Text.Parsec.Char (char, letter)
+import Text.Parsec.String (Parser)
 
 ------------------------------------------------------------------
--------------- 1) Recebendo uma fórmula como input ---------------
+-------------------------- Função Main ---------------------------
 ------------------------------------------------------------------
 
 main :: IO ()
 main = do
   -- Lê o arquivo "input.txt"
-  input <- readFile "inputs/input2.txt"
+  input <- readFile "inputs/input1.txt"
 
-  -- Converte o input p/ uma lista de tokens usando a função 'converterInputToken'
-  let tokens = converterInputToken input
-      formula = converterParaAST tokens -- Transforma a lista de tokens na AST usando a função 'converterParaAST'
-  putStrLn "Fórmula em Tokens:"
-  print formula
-
-  -- Simplifica a fórmula aplicando as regras do Tableau
-  let simplificada = aplicarRegras formula
-
-  putStrLn "Fórmula simplificada usando as regras do Tableau:"
-  print simplificada
+  -- Transformar input (string) num datatype (Formula)
+  -- utilizando 'case of' para fazer 'pattern matching' no resultado da função 'parseFormula input'
+  case parseFormula input of
+    Left erro -> print erro
+    Right formula -> do
+      putStrLn ("Fórmula Original: " ++ show formula)
+      -- Simplifica a fórmula aplicando as regras do Tableau
+      let formulaSimplificada = aplicarRegras formula
+      putStrLn ("Fórmula Simplificada Após Aplicação das Regras: " ++ show formulaSimplificada)
 
 ------------------------------------------------------------------
------- 2) Definindo uma estrutura pra cada token da fórmula ------
+---------- Definição da estrutura de dados da fórmula ------------
 ------------------------------------------------------------------
 
-data Token
-  = Variavel Char -- Variáveis proposicionais, representadas por caracteres
-  | E_Token -- Operador lógico "E" ^
-  | Ou_Token -- Operador lógico "OU" v
-  | Nao_Token -- Operador lógico "NÃO" ~
-  | Implicacao_Token -- Operador lógico "IMPLICA" ->
-  | BiImplicacao_Token -- Operador lógico "SE E SOMENTE SE" <->
-  | AbrirParenteses_Token -- Parêntese de abertura '('
-  | FecharParenteses_Token -- Parêntese de fechamento ')'
-  deriving (Show, Eq)
-
-------------------------------------------------------------------
---  3) Função que converte o input (que é uma string) em tokens --
-------------------------------------------------------------------
-
-converterInputToken :: String -> [Token]
-converterInputToken input =
-  if null input
-    then
-      []
-    else
-      if head input == '∧'
-        then
-          E_Token : converterInputToken (tail input)
-        else
-          if head input == '∨'
-            then
-              Ou_Token : converterInputToken (tail input)
-            else
-              if head input == '¬'
-                then
-                  Nao_Token : converterInputToken (tail input)
-                else
-                  if head input == '→'
-                    then
-                      Implicacao_Token : converterInputToken (tail input)
-                    else
-                      if head input == '↔'
-                        then
-                          BiImplicacao_Token : converterInputToken (tail input)
-                        else
-                          if head input == '('
-                            then
-                              AbrirParenteses_Token : converterInputToken (tail input)
-                            else
-                              if head input == ')'
-                                then
-                                  FecharParenteses_Token : converterInputToken (tail input)
-                                else
-                                  if isAlpha (head input)
-                                    then
-                                      Variavel (head input) : converterInputToken (tail input)
-                                    else
-                                      error ("Caractere inválido: " ++ [head input])
-
-------------------------------------------------------------------
---- 4) Transformar essa sequência de tokens numa estrutura de ----
---- dados 'fórmula' que representa a fórmula proposicional de ----
----                 maneira hierárquica                       ----
-------------------------------------------------------------------
-
--- Definição da estrutura de dados para a árvore de sintaxe abstrata (AST)
 data Formula
   = Var Char -- Variável proposicional representada por um caractere
   | Nao Formula -- Negação de uma fórmula
@@ -96,73 +35,50 @@ data Formula
   | BiImp Formula Formula -- Bi-implicação entre duas fórmulas
   deriving (Show, Eq)
 
--- PARSING
+-----------------------------------------------------------------
+--- Função principal para converter uma string em uma fórmula ---
+-----------------------------------------------------------------
+
 -- ORDEM DE PRECEDÊNCIA: ¬, ∧, ∨, →, ↔
 
--- Função para transformar a sequência de tokens na AST
-converterParaAST :: [Token] -> Formula
-converterParaAST tokens =
-  case converterExpressoes tokens of
-    (formula, []) -> formula
-    _ -> error "Tokens sobrando!"
+-- Função principal para converter uma string em uma fórmula
+parseFormula :: String -> Either ParseError Formula
+parseFormula = parse parseBiImplicacao ""
 
--- Função para analisar expressões considerando precedência e associatividade
-converterExpressoes :: [Token] -> (Formula, [Token])
-converterExpressoes tokens = parseBiImplicacao tokens
+-- Parser para variáveis
+parseVariavel :: Parser Formula
+parseVariavel = Var <$> letter
 
-parseBiImplicacao :: [Token] -> (Formula, [Token])
-parseBiImplicacao tokens =
-  let (esquerda, rest) = parseImplicacao tokens
-   in case rest of
-        (BiImplicacao_Token : rest') ->
-          let (direita, rest'') = parseBiImplicacao rest'
-           in (BiImp esquerda direita, rest'')
-        _ -> (esquerda, rest)
+-- Parser para parênteses
+parseParentese :: Parser Formula
+parseParentese = between (char '(') (char ')') parseBiImplicacao
 
-parseImplicacao :: [Token] -> (Formula, [Token])
-parseImplicacao tokens =
-  let (esquerda, rest) = parseOu tokens
-   in case rest of
-        (Implicacao_Token : rest') ->
-          let (direita, rest'') = parseImplicacao rest'
-           in (Imp esquerda direita, rest'')
-        _ -> (esquerda, rest)
+-- Parser para átomos (variável ou expressão entre parênteses)
+parseAtomo :: Parser Formula
+parseAtomo = parseVariavel <|> parseParentese
 
-parseOu :: [Token] -> (Formula, [Token])
-parseOu tokens =
-  let (esquerda, rest) = parseE tokens
-   in case rest of
-        (Ou_Token : rest') ->
-          let (direita, rest'') = parseOu rest'
-           in (Ou esquerda direita, rest'')
-        _ -> (esquerda, rest)
+-- Parser para negação
+parseNao :: Parser Formula
+parseNao = (char '¬' *> (Nao <$> parseNao)) <|> parseAtomo
 
-parseE :: [Token] -> (Formula, [Token])
-parseE tokens =
-  let (esquerda, rest) = parseNao tokens
-   in case rest of
-        (E_Token : rest') ->
-          let (direita, rest'') = parseE rest'
-           in (E esquerda direita, rest'')
-        _ -> (esquerda, rest)
+-- Parser para conjunção
+parseE :: Parser Formula
+parseE = chainl1 parseNao (E <$ (char '∧'))
 
-parseNao :: [Token] -> (Formula, [Token])
-parseNao (Nao_Token : rest) =
-  let (subFormula, rest') = parseAtomo rest
-   in (Nao subFormula, rest')
-parseNao tokens = parseAtomo tokens
+-- Parser para disjunção
+parseOu :: Parser Formula
+parseOu = chainl1 parseE (Ou <$ (char '∨'))
 
-parseAtomo :: [Token] -> (Formula, [Token])
-parseAtomo (Variavel x : rest) = (Var x, rest)
-parseAtomo (AbrirParenteses_Token : rest) =
-  let (subFormula, rest') = converterExpressoes rest
-   in case rest' of
-        (FecharParenteses_Token : rest'') -> (subFormula, rest'')
-        _ -> error "Parêntese de fechamento esperado!"
-parseAtomo _ = error "Token inesperado!"
+-- Parser para implicação
+parseImplicacao :: Parser Formula
+parseImplicacao = chainr1 parseOu (Imp <$ (string "→"))
+
+-- Parser para bi-implicação
+parseBiImplicacao :: Parser Formula
+parseBiImplicacao = chainr1 parseImplicacao (BiImp <$ (string "↔"))
 
 ------------------------------------------------------------------
----------------- 5) Definir as regras do Tableau -----------------
+------------------ Definir as regras do Tableau ------------------
 ------------------------------------------------------------------
 
 -- Aplicando as regras de decomposição para uma fórmula no Tableau
@@ -172,7 +88,7 @@ parseAtomo _ = error "Token inesperado!"
 
 -- Usa a função "simplificar" para aplicar uma regra, e se a fórmula resultante
 --      for igual à fórmula original, retorna a fórmula.
---      Caso contrário, aplicaas regras novamente à fórmula simplificada.
+--      Caso contrário, aplica as regras novamente à fórmula simplificada.
 
 aplicarRegras :: Formula -> Formula
 aplicarRegras formula =
@@ -195,18 +111,18 @@ simplificar (Nao (BiImp a b)) = Ou (E (simplificar a) (simplificar (Nao b))) (E 
 simplificar f = f -- Caso base: se nenhuma regra se aplica, retorna a fórmula como está
 
 ------------------------------------------------------------------
--------- 6) Definir uma estrutura de dados pro Tableau -----------
---------  para representar se o nó é interno ou folha  -----------
+---------- Definir uma estrutura de dados pro Tableau ------------
+---------- para representar se o nó é interno ou folha -----------
 ------------------------------------------------------------------
 
 ------------------------------------------------------------------
----- 7) Construir árvore do Tableau, o nó raiz será a negação ----
+-----  Construir árvore do Tableau, o nó raiz será a negação -----
 ---- da fórmula inicial, Deve-se expandir o nó aplicando as   ----
 ----     regras até que não seja mais possível expandir       ----
 ------------------------------------------------------------------
 
 ------------------------------------------------------------------
------- 8) Verificar se cada nó do ramo é fechado.          -------
+-------  Verificar se cada nó do ramo é fechado.          --------
 ------ Se todos forem fechados a fórmula é válida.         -------
 ------ Se no mínimo um nó for aberto a fórmula é inválida. -------
 ------------------------------------------------------------------
