a859f3aabcbdfc7612d3220adf49e345a1e174ce
oftheus <matheusgs071@gmail.com>
2024-06-08 19:02:13 -0300

    v 1.6
    v 1.6

--- main.hs
+++ main.hs
@@ -18,9 +18,6 @@ main = do
     Left erro -> print erro
     Right formula -> do
       putStrLn ("Fórmula Original: " ++ show formula)
-      -- Simplifica a fórmula aplicando as regras do Tableau
-      let formulaSimplificada = aplicarRegras formula
-      putStrLn ("Fórmula Simplificada Após Aplicação das Regras: " ++ show formulaSimplificada)
 
 ------------------------------------------------------------------
 ---------- Definição da estrutura de dados da fórmula ------------
@@ -32,22 +29,16 @@ data Formula
   | E Formula Formula -- Conjunção (E) de duas fórmulas
   | Ou Formula Formula -- Disjunção (OU) de duas fórmulas
   | Imp Formula Formula -- Implicação de uma fórmula para outra
-  | BiImp Formula Formula -- Bi-implicação entre duas fórmulas
   deriving (Show, Eq)
 
 -----------------------------------------------------------------
 --- Função principal para converter uma string em uma fórmula ---
 -----------------------------------------------------------------
 
--- ORDEM DE PRECEDÊNCIA: ¬, ∧, ∨, →, ↔
+-- ORDEM DE PRECEDÊNCIA: ¬, ∧, ∨, →
 
--- Função principal para converter uma string em uma fórmula
 parseFormula :: String -> Either ParseError Formula
-parseFormula = parse parseBiImplicacao ""
-
--- Parser para bi-implicação
-parseBiImplicacao :: Parser Formula
-parseBiImplicacao = chainr1 parseImplicacao (BiImp <$ (string "↔"))
+parseFormula = parse parseImplicacao ""
 
 -- Parser para implicação
 parseImplicacao :: Parser Formula
@@ -75,39 +66,25 @@ parseVariavel = Var <$> letter
 
 -- Parser para parênteses
 parseParentese :: Parser Formula
-parseParentese = between (char '(') (char ')') parseBiImplicacao
+parseParentese = between (char '(') (char ')') parseImplicacao
 
 ------------------------------------------------------------------
 ------------------ Definir as regras do Tableau ------------------
 ------------------------------------------------------------------
 
--- Aplicando as regras de decomposição para uma fórmula no Tableau
-
--- Função "aplicarRegras" tenta simplificar uma fórmula aplicando repetidamente
--- as regras de decomposição até que a fórmula não mude mais.
-
--- Usa a função "simplificar" para aplicar uma regra, e se a fórmula resultante
+-- Usar a função "simplificar" para aplicar uma regra, e se a fórmula resultante
 --      for igual à fórmula original, retorna a fórmula.
 --      Caso contrário, aplica as regras novamente à fórmula simplificada.
 
-aplicarRegras :: Formula -> Formula
-aplicarRegras formula =
-  let simplificado = simplificar formula
-   in if formula == simplificado
-        then formula
-        else aplicarRegras simplificado
-
--- Simplificando a fórmula aplicando as regras
+-- Simplificar a fórmula aplicando as regras
 simplificar :: Formula -> Formula
-simplificar (Nao (Nao a)) = simplificar a -- ~(~A) = B
-simplificar (Ou a b) = Ou (simplificar a) (simplificar b) -- A v
+simplificar (Nao (Nao a)) = simplificar a -- ~(~A) = A
+simplificar (Ou a b) = Ou (simplificar a) (simplificar b) -- A v B
 simplificar (E a b) = E (simplificar a) (simplificar b) -- A ^ B
 simplificar (Nao (Ou a b)) = E (simplificar (Nao a)) (simplificar (Nao b)) -- ~(A v B) = ~A ^ ~B
 simplificar (Nao (E a b)) = Ou (simplificar (Nao a)) (simplificar (Nao b)) -- ~(A ^ B) = ~A v ~B
 simplificar (Imp a b) = Ou (simplificar (Nao a)) (simplificar b) -- (A -> B) = ~A v B
-simplificar (BiImp a b) = Ou (E (simplificar a) (simplificar b)) (E (simplificar (Nao a)) (simplificar (Nao b))) -- (A <-> B) = (A ^ B) v (~A ^ ~B)
 simplificar (Nao (Imp a b)) = E (simplificar a) (simplificar (Nao b)) -- ~(A -> B) = A ^ ~B
-simplificar (Nao (BiImp a b)) = Ou (E (simplificar a) (simplificar (Nao b))) (E (simplificar (Nao a)) (simplificar b)) -- ~(A <-> B) = (A ^ ~B) v (~A ^ B)
 simplificar f = f -- Caso base: se nenhuma regra se aplica, retorna a fórmula como está
 
 ------------------------------------------------------------------
@@ -115,11 +92,15 @@ simplificar f = f -- Caso base: se nenhuma regra se aplica, retorna a fórmula c
 ---------- para representar se o nó é interno ou folha -----------
 ------------------------------------------------------------------
 
+
+
 ------------------------------------------------------------------
 -----  Construir árvore do Tableau, o nó raiz será a negação -----
 ---- da fórmula inicial, Deve-se expandir o nó aplicando as   ----
 ----     regras até que não seja mais possível expandir       ----
 ------------------------------------------------------------------
+
+
 
 ------------------------------------------------------------------
 -------  Verificar se cada nó do ramo é fechado.          --------
